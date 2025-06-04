cc4dca793d22757d045f9c9a12bf6cd53d0532df
Julien Boudry <julien.boudry@gmail.com>
2025-06-04 14:51:01 +0200

    Add entry point and application class for PHP Reference Documentation Generator

---
+++ bin/entrypoint.php
@@ -0,0 +1,8 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+use JulienBoudry\PhpReference\App;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . '../vendor/autoload.php';
+
+App::runApplication();
---
+++ src/App.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace JulienBoudry\PhpReference;
+
+use JulienBoudry\PhpReference\Command\GenerateDocumentationCommand;
+
+abstract class App
+{
+    public const string VERSION = '1.0.0';
+    public const string NAME = 'PHP Reference Documentation Generator';
+
+    public static function getFullName(): string
+    {
+        return sprintf('%s v%s', self::NAME, self::VERSION);
+    }
+
+    public static function runApplication(): void
+    {
+        $application = new \Symfony\Component\Console\Application(
+            name : self::NAME,
+            version: self::VERSION,
+        );
+
+        $generateCommand = new GenerateDocumentationCommand();
+
+        $application->add($generateCommand);
+        $application->setDefaultCommand($generateCommand->getName(), true);
+
+        $application->run();
+    }
+}
